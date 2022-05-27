d9c2a6f31f868481f2bf1b6b1d2b30e42ff1c760
mrmagic2020 <104749673+mrmagic2020@users.noreply.github.com>
2022-05-27 10:16:38 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const logger = new NIL.Logger(`test`);
 const path = require(`path`);
 const temporary_conf = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'tmpconf.json')));
-// const temporary_list = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'tmplist.json')))
+const temporary_list = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'tmplist.json')));
+
 
 function checkFile(file, text) {
     if (NIL.IO.exists(path.join(__dirname, file)) == false) {
@@ -15,32 +16,34 @@ const config = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'config.json')));
 const detect_curse = config.detect_curse;
 const detect_sensitive = config.detect_sensitive;
 const detect_curse_extensions = config.detect_curse_extensions;
+const detect_custom = config.detect_curse;
 
-// 转移list数据（regex）-暂时无法实现
-// checkFile("list.json", JSON.stringify(temporary_list, null, '\t'));
-// const list = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'list.json')));
-// let curse_extensions_arr_tmp = list.curse_extensions_arr;
-// const curse_extensions_arr = new RegExp(curse_extensions_arr_tmp);
 
-// 需要检测的正则表达式列表
-// 格式：(.*)xxx(.*)|(.*)yyy(.*)
-// (.*)表示"xxx""yyy"前后可以有其它字符，去掉后仅匹配单独发出的"xxx""yyy"
-// "｜"表示选择，将不同的关键词用"｜"分开即可
-// 更多正则表达式语法见：https://www.runoob.com/regexp/regexp-syntax.html
+// 转移list数据（regex）
+checkFile("list.json", JSON.stringify(temporary_list, null, '\t'));
+const list = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'list.json')));
 
-// 脏话屏蔽列表
-let curse_arr = new RegExp(`(.*)他奶奶的(.*)|(.*)肏(.*)|(.*)操(.*)|(.*)傻逼(.*)|(.*)妈逼(.*)|(.*)贱逼(.*)|(.*)他妈的(.*)|(.*)王八蛋(.*)|(.*)狗日(.*)|(.*)混蛋(.*)|(.*)滚蛋(.*)|(.*)贱人(.*)SB(.*)|(.*)sb(.*)|(.*)JB(.*)|(.*)jb(.*)|(.*)鸡巴(.*)|(.*)fuck(.*)|(.*)bitch(.*)|(.*)fucking(.*)|(.*)fucker(.*)|(.*)asshole(.*)|(.*)bastard(.*)|(.*)damn(.*)`);
+let curse_arr_tmp = list.curse_arr;
+let curse_arr = new RegExp(curse_arr_tmp);
 
-// 敏感词汇屏蔽列表
-let sensitive_arr = new RegExp(`(.*)港独(.*)|(.*)香港独立(.*)|(.*)台独(.*)|(.*)台湾独立(.*)|(.*)VPN(.*)|(.*)vpn(.*)`);
+let sensitive_arr_tmp = list.sensitive_arr;
+let sensitive_arr = new RegExp(sensitive_arr_tmp);
 
-// 替代脏话词汇屏蔽列表
-let curse_extensions_arr = new RegExp(`(.*)焯(.*)|(.*)淦(.*)|草|(.*)艹(.*)|(.*)沃日(.*)|(.*)我日(.*)|(.*)CAO(.*)|(.*)cao(.*)|(.*)草泥马(.*)|(.*)草尼玛(.*)|(.*)md(.*)|(.*)tmd(.*)|(.*)tnnd(.*)|(.*)cnm(.*)`);
+let curse_extensions_arr_temp = list.curse_extensions_arr;
+let curse_extensions_arr = new RegExp(curse_extensions_arr_temp);
+
+let customs_arr_tmp = list.customs_arr;
+let customs_arr = new RegExp(customs_arr_tmp);
+
 
 // 脏话/敏感词等回复内容
-let curse_reply_arr = [`不许骂人！`,`谁在骂人🤨`,`请文明用语！`,`啊？你说啥？撤回太快没看见🤪`,`骂谁呢！`,`喂？宛平南路600号？`,`再骂，就……就……让群主把你禁言😠`]
-let sensitive_reply_arr = [`已屏蔽敏感词汇！`,`小心你被举办（`,`喂？110？`,`政治要正确！doge🤐`]
-let curse_extensions_reply_arr = [`换个词就不叫骂人了？`,`别以为我不识字！`,`啧也是脏话！`]
+let curse_reply_arr = [`不许骂人！`,`谁在骂人🤨`,`请文明用语！`,`啊？你说啥？撤回太快没看见🤪`,`骂谁呢！`,`喂？宛平南路600号？这有个口吐芬芳的😝`,`再骂，就……就……让群主把你禁言😠`];
+
+let sensitive_reply_arr = [`已屏蔽敏感词汇！`,`小心你被举办（`,`喂？110？这人反动！`,`政治要正确！doge🤐`];
+
+let curse_extensions_reply_arr = [`换个词就不叫骂人了？`,`别以为我不识字！这也是脏话😣`,`啧也是脏话！`,`文明交流！`];
+
+let customs_reply_arr = [`群主不许你这么说话！`]
 
 class example extends NIL.ModuleBase{
     onStart(api){
@@ -63,6 +66,12 @@ class example extends NIL.ModuleBase{
                 if(detect_curse_extensions == true){
                     e.recall();
                     e.reply(curse_extensions_reply_arr[Math.floor(Math.random()*curse_extensions_reply_arr.length)])
+                }
+            }
+            if(customs_arr.test(e.raw_message) == true){
+                if(detect_custom == true){
+                    e.recall();
+                    e.reply(customs_reply_arr[Math.floor(Math.random()*customs_reply_arr.length)])
                 }
             }
         })}
